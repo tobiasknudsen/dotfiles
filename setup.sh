85e3879f77829d93d282ddf3d41f80b64a2568f6
Tobias Knudsen <tobias.hartvedt.knudsen@gmail.com>
2024-10-23 09:33:52 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,6 +19,17 @@ echo "Git config"
 git config --global user.name "Tobias Knudsen"
 git config --global user.email tobias.hartvedt.knudsen@gmail.com
 git config --global pull.rebase true
+git config --global push.autoSetupRemote true
+
+# This should already be done since this file is in the dotfiles repo
+# echo "Getting dotfiles..."
+# alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
+# git clone --bare git@github.com:tobiasknudsen/dotfiles.git $HOME/.dotfiles.git
+# dotfiles checkout
+# dotfiles config --local status.showUntrackedFiles no
+
+echo "Set up SSH with 1password"
+read -p "Press [Enter] when finished..."
 
 echo "Installing other brew stuff..."
 brew install fzf
@@ -28,6 +39,8 @@ brew install direnv
 brew install precommit
 brew install xz #(To make python from asdf work)
 brew install terraform
+brew install poetry
+brew install bruno
 
 echo "Install asdf..."
 brew install asdf
@@ -43,19 +56,6 @@ asdf install nodejs latest
 echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-echo "Set up SSH with 1password"
-read -p "Press [Enter] when finished..."
-
-echo "Getting dotfiles..."
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME"
-git clone --bare git@github.com:tobiasknudsen/dotfiles.git $HOME/.dotfiles.git
-dotfiles checkout
-dotfiles config --local status.showUntrackedFiles no
-
-echo "Installing zsh plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
-
 
 apps=(
     raycast
@@ -65,13 +65,17 @@ apps=(
     slack
     bettertouchtool
     simplenote
-    vscodium
     shottr
     cursor
+    1password
 )
 
 echo "installing apps with Cask..."
 brew cask install --appdir="/Applications" ${apps[@]}
+
+echo "Installing zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/plugins/zsh-syntax-highlighting
 
 echo "Create symlink for settings and keybindings"
 rm ~/Library/Application\ Support/Cursor/User/settings.json
@@ -85,7 +89,6 @@ curl -L https://github.com/davidwernhart/AlDente-Charge-Limiter/releases/downloa
 curl -L https://updates.topnotch.app/TopNotch-latest.zip --output ~/Downloads/TopNotch-latest.zip
 curl -L https://download.bjango.com/istatmenus5/ --output ~/Downloads/istatmenus5.zip
 curl -L https://macrelease.matthewpalmer.net/Vanilla.dmg --output ~/Downloads/Vanilla.dmg
-curl -L https://github.com/menubar-apps/PullBar/releases/download/v1.5/pullBar.1.5.dmg --output ~/Downloads/pullBar.1.5.dmg
 
 printf 'Set Mac OS settings(y/n)? '
 read answer
@@ -116,6 +119,10 @@ if [ "$answer" != "${answer#[Yy]}" ] ;then
 
     #"Avoiding the creation of .DS_Store files on network volumes"
     defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+    # Save screenshots to ~/Screenshots
+    mkdir ~/Screenshots
+    defaults write com.apple.screencapture "location" -string "~/Screenshots" && killall SystemUIServer
 
     killall Finder
 fi
