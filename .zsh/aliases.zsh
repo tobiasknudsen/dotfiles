5afdd17bde05827531d063f39a455c21b47fadd6
Tobias Knudsen <tobias.hartvedt.knudsen@gmail.com>
2024-10-23 08:46:25 +0200

    Add tienda aliases that support multiple tenants

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,12 +8,32 @@ alias vær="finger 0475@graph.no | grep -v NRK"
 # Run black, isort, flake8 on all changed files
 alias pretty="git ls-files '*.py' -m | xargs -I % sh -c 'black %; isort %; flake8 %'"
 
-alias gl="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
-alias klipy="~/dev/klipy/.venv/bin/python -m klipy"
+# alias gl="git log --pretty=format:'%C(yellow)%h %Cred%ad %Cblue%an%Cgreen%d %Creset%s' --date=short"
 
 # Django
-alias runserver="~/dev/tienda/.venv/bin/python manage.py runserver 0.0.0.0:8000"
-alias makemigrations="~/dev/tienda/.venv/bin/python manage.py makemigrations"
-alias migrate="~/dev/tienda/.venv/bin/python manage.py migrate"
-alias shell="~/dev/tienda/.venv/bin/python manage.py shell_plus"
+alias runserver='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ~/dev/tienda/.venv/bin/python manage.py runserver 0.0.0.0:8000'
+alias makemigrations='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ~/dev/tienda/.venv/bin/python manage.py makemigrations'
+alias showmigrations='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ~/dev/tienda/.venv/bin/python manage.py showmigrations'
+alias migrate='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ~/dev/tienda/.venv/bin/python manage.py migrate'
+alias shell='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ~/dev/tienda/.venv/bin/python manage.py shell_plus'
+alias update-db='DJANGO_SETTINGS_MODULE="tienda.settings_local" POSTGRES_DB="tienda" TENANT="no" ./bin/update-db'
+
+alias runserver:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ~/dev/tienda/.venv/bin/python manage.py runserver 0.0.0.0:8000'
+alias makemigrations:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ~/dev/tienda/.venv/bin/python manage.py makemigrations'
+alias showmigrations:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ~/dev/tienda/.venv/bin/python manage.py showmigrations'
+alias migrate:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ~/dev/tienda/.venv/bin/python manage.py migrate'
+alias shell:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ~/dev/tienda/.venv/bin/python manage.py shell_plus'
+alias update-db:se='DJANGO_SETTINGS_MODULE="tienda.settings_local_se" POSTGRES_DB="tienda_se" TENANT="se" ./bin/update-db'
+
+
 alias generate="~/dev/tienda/.venv/bin/python manage.py generate_api_schemas ; npm run openapi:generate"
+
+# Tienda web
+alias start:no='NEXT_PUBLIC_TENANT="no" TIENDA_INTERNAL_HOSTNAME="http://oda.localhost" TIENDA_DOMAIN="oda.localhost" TIENDA_HOSTNAME="http://oda.localhost" TIENDA_WEB_HOSTNAME="http://oda.localhost" X_CLIENT_TOKEN="OeD5WJnGuY3JxAF5rs61cjdjuvUfKplN7izeeU5MoH3HsNwol4" npm run start:no'
+alias start:se='NEXT_PUBLIC_TENANT="se" TIENDA_INTERNAL_HOSTNAME="http://mathem.localhost" TIENDA_DOMAIN="mathem.localhost" TIENDA_HOSTNAME="http://mathem.localhost" TIENDA_WEB_HOSTNAME="http://mathem.localhost" X_CLIENT_TOKEN="IJxqnIXYOzxP7ZfAxnKUTLsSwEvn1W6iDYHRg2WBuAOW7zw7qI" npm run start:se'
+# Remove zsh aliases
+unalias ga
+unalias gr
+unalias gap
+unalias gl
+
